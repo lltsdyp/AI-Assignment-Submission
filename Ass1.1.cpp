ff4ac8e5874ed27938eefbd334185e8499cfea6b
Jizimo <jizimo0430@outlook.com>
2025-03-21 11:05:16 +0800

    test

--- Ass1.1.cpp
+++ Ass1.1.cpp
@@ -5,49 +5,49 @@
 
 using namespace std;
 
-constexpr int N=1e5+19;
+constexpr int N = 1e5 + 19;
 
-class status{
+class status
+{
 public:
     int pos;
     int dist;
 };
 
-int main(){
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n,m;
+    int n, m;
     queue<status> q;
-    q.push({1,0});
-    cin>>n>>m;
-    vector<vector<int>> graph(n+1,vector<int>(n+1,0));
-    vector<int> visited(n+1,0);
-    for(int _=0;_<m;++_)
+    q.push({1, 0});
+    cin >> n >> m;
+    vector<vector<int>> graph(n + 1);
+    vector<int> visited(n + 1, 0);
+    for (int _ = 0; _ < m; ++_)
     {
-        int u,v;
-        cin>>u>>v;
-        graph[u][v]=1;
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
     }
-    while(!q.empty())
+    while (!q.empty())
     {
-        status cur=q.front();
+        status cur = q.front();
         q.pop();
-        if(visited[cur.pos])
+        if (visited[cur.pos])
             continue;
-        visited[cur.pos]=1;
-        if(cur.pos==n)
+        visited[cur.pos] = 1;
+        if (cur.pos == n)
         {
-            cout<<cur.dist<<'\n';
+            cout << cur.dist << '\n';
             return 0;
         }
-        for(int i=1;i<=n;++i)
+        // 遍历邻接节点
+        for (int neighbor : graph[cur.pos])
         {
-            if(graph[cur.pos][i]==1)
-            {
-                q.push({i,cur.dist+1});
-            }
+            q.push({neighbor, cur.dist + 1});
         }
     }
-    cout<<-1<<'\n';
+    cout << -1 << '\n';
     return 0;
 }
