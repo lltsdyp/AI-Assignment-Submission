75bd10eeeafb35c69a365e34f775bd004eb0f818
Jizimo <jizimo0430@outlook.com>
2025-03-28 10:20:26 +0800

    Fixup-1

--- Ass1.2.cpp
+++ Ass1.2.cpp
@@ -1,46 +1,48 @@
-#include <iostream>
 #include <climits>
+#include <iostream>
 
 using namespace std;
 
-constexpr int N=9999;
+constexpr int N = 9999;
 int graph[N][N];
 int vis[N];
 int dis[N];
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n,m;
-    cin>>n>>m;
-    dis[1]=0;
-    for(int i=2;i<=n;++i)
-    {
-        dis[i]=INT_MAX>>1; //防止加爆了
-    }
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  dis[1] = 0;
+  for (int i = 2; i <= n; ++i) {
+    dis[i] = INT_MAX >> 1; // 防止加爆了
+  }
 
-    for(int _=0;_<m;++_)
-    {
-        int u,v,cost;
-        cin>>u>>v>>cost;
-        if(graph[u][v]==0 || cost<graph[u][v])
-            graph[u][v]=cost;
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j < n; ++j) {
+      graph[i][j] = INT_MAX >> 1;
     }
-    for(int i=1;i<=n;++i)
-    {
-        int minNode=-1;
-        if(!vis[i] && (dis[i]<dis[minNode] || minNode==-1))
-            minNode=i;
-        vis[minNode]=1;
-        for(int j=1;j<=n;++j)
-        {
-            if(graph[minNode][j]){
-                int newDis=dis[minNode]+graph[minNode][j];
-                if(newDis<dis[j])
-                    dis[j]=newDis;
-            }
-        }
+  }
+
+  for (int _ = 0; _ < m; ++_) {
+    int u, v, cost;
+    cin >> u >> v >> cost;
+    if (cost < graph[u][v])
+      graph[u][v] = cost;
+  }
+  for (int i = 1; i <= n; ++i) {
+    int minNode = -1;
+    for (int j = 1; j <= n; ++j) {
+      if (!vis[i] && (minNode == -1 || dis[i] < dis[minNode]))
+        minNode = i;
     }
-    cout<<(dis[n]==INT_MAX>>1?-1:dis[n])<<'\n';
-    return 0;
+    vis[minNode] = 1;
+    for (int j = 1; j <= n; ++j) {
+      int newDis = dis[minNode] + graph[minNode][j];
+      if (newDis < dis[j])
+        dis[j] = newDis;
+    }
+  }
+  cout << (dis[n] == INT_MAX >> 1 ? -1 : dis[n]) << '\n';
+  return 0;
 }
